f8d951d0ad7d2aa0b497c0404e11e7069ce95b33
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-05-01 21:36:54 -0300

    Escopos de beans

--- src/main/java/com/algaworks/cursojavaee/controller/PrecoProdutoBean.java
+++ src/main/java/com/algaworks/cursojavaee/controller/PrecoProdutoBean.java
@@ -1,15 +1,36 @@
 package com.algaworks.cursojavaee.controller;
 
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+//import javax.faces.bean.RequestScoped;		//do JSF
+import javax.enterprise.context.RequestScoped;  //do CDI
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.cursojavaee.service.CalculadoraPrecoService;
 
 @Named("meuBean")
-public class PrecoProdutoBean {
+//@Dependent 			//do CDI - é o padrão -- assume o escpo de quem  o chama
+//@RequestScoped		//dura uma requisição HTTP
+//@SessionScoped
+//@ViewScoped			//Não existe no CDI
+public class PrecoProdutoBean implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Inject
 	private CalculadoraPrecoService calculadoraPrecoService;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Init ProdutoBean");
+	}
 	
 	public double getPreco() {
 		return calculadoraPrecoService.CalculaPreco(12, 44.55);
--- src/main/java/com/algaworks/cursojavaee/service/CalculadoraPrecoService.java
+++ src/main/java/com/algaworks/cursojavaee/service/CalculadoraPrecoService.java
@@ -1,6 +1,17 @@
 package com.algaworks.cursojavaee.service;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped 
 public class CalculadoraPrecoService {
+	
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Init CalculadoraPrecoService");
+	}
 	
 	public double CalculaPreco(int qtde, double precoUni) {
 		return qtde * precoUni;
